c2ea0c3b98dc7db5218aa7ceca9196e883ce669d
wdlin233 <wdlin233@163.com>
2024-12-09 19:49:10 +0800

    hw7-4 passed

--- hw7-4.cpp
+++ hw7-4.cpp
@@ -2,32 +2,34 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
+#include <set>
 
 using namespace std;
 
 int D = 0;
 int M = 0, N = 0;
 //int G[100005][100005] = {0};
-vector<int> G[100005];
-int in[100005];
-bool toposort(int n);
+//vector<set<int>> G[100005];
+//int in[100005];
+bool toposort(vector<set<int>>& G, vector<int>& in, int n);
 
 int main(void) {
     cin >> D;
     while(D > 0) {
         cin >> N >> M;
-        int u, v;
-        for (int i = 1; i <= N; i++) {
-            G[i].clear();
-            in[i] = 0;
-        }
-        for(int i = 0; i < M; i++) {
-            cin >> u >> v;
-            if (u == v) continue;
-            else {
-                G[u].push_back(v);
-                in[v]++;
-            }
+        vector<set<int>>  graph(N + 1);
+        vector<set<int>>  graph2(N + 1);
+        vector<int>  deDegree(N + 1, 0);
+    
+        // why should I use inverse toposort?
+        // because you need to decide which node as output firstly
+        // depending on its next node(make it smaller) 
+        for (int i = 0; i < M; i++)  {
+          int  from, to;
+          cin >> from >> to;
+          graph[from].insert(to);
+          graph2[to].insert(from);
+          deDegree[from] = graph[from].size();
         }
         // for(int i = 0; i < M; i++) {
         //     cin >> u >> v;
@@ -35,18 +37,18 @@ int main(void) {
         //     //G[v][u]++;
         //     in[v]++;
         // }
-        if (!toposort(N)) cout << "Impossible!" << endl;
+
+        if (!toposort(graph2, deDegree, N)) cout << "Impossible!" << endl;
         //G[100005] = {0};
         D--;
     }
     return 0;
 }
 
-bool toposort(int n) {
+bool toposort(vector<set<int>>& G, vector<int>& in, int n) {
   vector<int> L;
-  //queue<int> S;
-  priority_queue<int, vector<int>, greater<int>> S;
-  //priority_queue<int> S;
+  //priority_queue<int, vector<int>, greater<int>> S;
+  priority_queue<int> S; // maxheap
   for (int i = 1; i <= n; i++)
     if (in[i] == 0) S.push(i);
   while (!S.empty()) {
@@ -61,7 +63,9 @@ bool toposort(int n) {
     }
   }
   if (L.size() == n) {
+    reverse(L.begin(), L.end());
     for (auto i : L) cout << i << ' ';
+    //for(int i = L.size() - 1; i >= 0; i--) cout << L[i] << ' ';
     cout << endl;
     return true;
   }
