76d1a0dcc0f1f8c94b8d0c047e6649ce920e0703
wdlin233 <wdlin233@163.com>
2024-12-26 16:53:40 +0800

    lab5 didnt pass medium test

--- lab5/src/heap.cpp
+++ lab5/src/heap.cpp
@@ -4,44 +4,127 @@
 
 #define LIST_SIZE 2000
 
+void swap(Player_info &a, Player_info &b) {
+    Player_info temp = a;
+    a = b;
+    b = temp;
+}
+
 Heap::~Heap() {
     //TODO
+    delete[] arr;
+    num = 0;
 }
 
 Heap::Heap(Heap &other) {
     //TODO
+    arr = new Player_info[LIST_SIZE];
+    for (int i = 0; i < LIST_SIZE; i++) {
+        arr[i] = other.arr[i];
+    }
+    num = other.num;
 }
 
 Heap &Heap::operator=(Heap &other) {
     //TODO
+    arr = new Player_info[LIST_SIZE];
+    for (int i = 0; i < LIST_SIZE; i++) {
+        arr[i] = other.arr[i];
+    }
+    num = other.num;
+    return *this;
 }
 
 void Heap::heapify_up() {
-    //TODO
+    int index = num - 1;
+    while (index > 0) {
+        int parent = get_father(index);
+        if (arr[index] < arr[parent]) {
+            swap(arr[index], arr[parent]);
+            index = parent;
+        } else {
+            break;
+        }
+    }
 }
 
 void Heap::heapify_down() {
-    //TODO
+    int index = 0;
+    while (true) {
+        int left_child = get_child(LEFT, index);
+        int right_child = get_child(RIGHT, index);
+        int smallest = index;
+
+        if (left_child != -1 && arr[left_child] < arr[smallest]) {
+            smallest = left_child;
+        }
+        if (right_child != -1 && arr[right_child] < arr[smallest]) {
+            smallest = right_child;
+        }
+        
+        if (smallest != index) {
+            swap(arr[index], arr[smallest]);
+            index = smallest;
+        } else {
+            break;
+        }
+    }
 }
 
 int Heap::get_child(bool direction, int index) {
     //TODO
-    return -1;
+    int child = direction == LEFT ? 2 * index + 1 : 2 * index + 2;
+    if (child < num) {
+        return child;
+    } else {
+        return -1;
+    }
 }
 
 int Heap::get_father(int index) {
     //TODO
-    return -1;
+    int father = (index - 1) / 2;
+    if (father < 0 || father >= num) {
+        return -1;
+    }
+    else {
+        return father;
+    }
 }
 
 int Heap::insert(Player_info &p) {
-    //TODO
-    return -1;
+    if (num < 2000) {
+        arr[num] = p;
+        heapify_up();
+        num += 1;
+        return 1;
+    }
+    else {
+        if (p > arr[0]) {
+            arr[0] = p;
+            heapify_down();
+            return 1;
+        }
+    }
+    return 0;
 }
 
 Player_info *Heap::sort() {
     //TODO
-    return NULL;
+    int n = num;
+    Player_info *vec = new Player_info[2000];
+    for (int i = 0; i < num; i++) {
+        num--;
+        if (num <= 0)
+            break;
+        swap(arr[0], arr[num]);
+        heapify_down();
+    }
+    num = n;
+    for (int i = 0; i < 2000; i++) {
+        vec[i] = arr[i];
+    }
+    return vec;
 }
 
 /*
